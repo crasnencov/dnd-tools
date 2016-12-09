56cbec17aff7d3ec4e376e58a89708aa3e4f7cd3
crasnencov <crasnencov@gmail.com>
2016-12-09 15:04:10 +0200

    Tried to count strength modifier

--- app/character-generator/character-generator.controller.js
+++ app/character-generator/character-generator.controller.js
@@ -1,31 +1,42 @@
 'use strict';
-(function(){
-  function CharacterGeneratorCtrl ($scope, CharacterService) {
+(function () {
+  function CharacterGeneratorCtrl($scope, CharacterService) {
     var vm = this;
 
-    function init(){
+    function init() {
       CharacterService.initCharacter();
+      // CharacterService.calculateModifiers();
       vm.character = CharacterService.character;
     }
 
-
-
-    console.log(CharacterService.character);
-    console.log($scope);
-
     init();
+
+    $scope.$watch(function () {
+      return vm.character.abilities;
+    }, function (newValue, oldValue) {
+      console.log('new value ' + newValue);
+      vm.character.modifiers.strength = Math.floor((newValue - 10) / 2);
+      console.log(arguments);
+      console.log('calculate modifiers ' + vm.character.modifiers.s);
+    }, true);
+
+    vm.onClickModifiers = function () {
+      console.log($scope);
+      CharacterService.calculateModifiers();
+      $scope.myVar = ++$scope.myVar;
+    };
   }
 
   angular.module('myApp.CharacterGenerator', ['ngRoute'])
 
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/character-generator', {
-      templateUrl: 'character-generator/character-generator.html',
-      controllerAs: 'CharacterGeneratorCtrl'
-    });
-  }])
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.when('/character-generator', {
+        templateUrl: 'character-generator/character-generator.html',
+        controllerAs: 'vm'
+      });
+    }])
 
-  .controller('CharacterGeneratorCtrl',
+    .controller('CharacterGeneratorCtrl',
     ['$scope', 'CharacterService', CharacterGeneratorCtrl]
   );
 
--- app/service/character.service.js
+++ app/service/character.service.js
@@ -1,12 +1,19 @@
 'use strict';
-(function(){
-  function CharacterService () {
+(function () {
+  function CharacterService() {
 
     return {
-
-      initCharacter: function() {
+      initCharacter: function () {
         this.character = {
           abilities: {
+            strength: 25,
+            dexterity: 14,
+            constitution: 43,
+            intelligence: 56,
+            wisdom: 12,
+            charisma: 43
+          },
+          modifiers: {
             strength: 0,
             dexterity: 0,
             constitution: 0,
@@ -15,10 +22,21 @@
             charisma: 0
           }
         };
+      },
+      calculateModifiers: function () {
+        var _this = this;
+        angular.forEach(this.character.abilities, function (value, key) {
+          _this.character.modifiers[key] = Math.floor((value - 10) / 2);
+          console.log(key + ': ' + value);
+        });
+        console.log(this.character.modifiers); //angular.foreach
       }
 
     }
+
   }
+
 
   angular.module('myApp').factory('CharacterService', [CharacterService]);
 })();
+
